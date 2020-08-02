7bb428e2164aa1f9b1c4f9f3f148c37f838045d3
wnuke <wnuke@wnuke.dev>
2020-08-02 19:20:24 +0200

    add documentation for minecraft api send and recieve templates

--- src/main/kotlin/dev/wnuke/botmanager/minecraft/APIResponseTemplates.kt
+++ src/main/kotlin/dev/wnuke/botmanager/minecraft/APIResponseTemplates.kt
@@ -1,9 +1,67 @@
 package dev.wnuke.botmanager.minecraft
 
-data class Chat(val messages: ArrayList<String>)
+/**
+ * Template for chat get request responses from the Minecraft API
+ */
+data class Chat(
+        /**
+         * ArrayList of the recieved messages
+         */
+        val messages: ArrayList<String>)
 
-data class Player(val name: String, val uuid: String, val player: PlayerInfo, val coordinates: Position)
+/**
+ * Template for player information request responses from the Minecraft API
+ */
+data class Player(
+        /**
+         * Name of the logged in account
+         */
+        val name: String,
+        /**
+         * UUID of the logged in account
+         */
+        val uuid: String,
+        /**
+         * More details on the
+         */
+        val player: PlayerInfo,
+        /**
+         * Players current location in the world
+         */
+        val coordinates: Position)
 
-data class PlayerInfo(val health: Float, val hunger: Float, val saturation: String)
+/**
+ * Data class to complement the Player class
+ * @see Player
+ */
+data class PlayerInfo(
+        /**
+         * Health level
+         */
+        val health: Float,
+        /**
+         * Hunger level
+         */
+        val hunger: Float,
+        /**
+         * Saturation level
+         */
+        val saturation: String)
 
-data class Position(val x: Double, val y: Double, val z: Double)
+/**
+ * Data class to complement the Player class, contains the in world position of the player
+ * @see Player
+ */
+data class Position(
+        /**
+         * X coordinate
+         */
+        val x: Double,
+        /**
+         * Y coordinate
+         */
+        val y: Double,
+        /**
+         * Z coordinate
+         */
+        val z: Double)
--- src/main/kotlin/dev/wnuke/botmanager/minecraft/APISendTemplates.kt
+++ src/main/kotlin/dev/wnuke/botmanager/minecraft/APISendTemplates.kt
@@ -2,11 +2,40 @@ package dev.wnuke.botmanager.minecraft
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Template for sending a server connect request to the Minecraft API
+ */
 @Serializable
-data class ServerConnect(val address: String, val port: String)
+data class ServerConnect(
+        /**
+         * Address of the server to connect to
+         */
+        val address: String,
+        /**
+         * Port to connect to
+         */
+        val port: String)
 
+/**
+ * Template for sending a message send request to the Minecraft API
+ */
 @Serializable
-data class Message(val message: String)
+data class Message(
+        /**
+         * Message to send
+         */
+        val message: String)
 
+/**
+ * Template for sending a login request to the Minecraft API
+ */
 @Serializable
-data class Login(val username: String, val password: String)
+data class Login(
+        /**
+         * Username to login as
+         */
+        val username: String,
+        /**
+         * Password to use for authentication
+         */
+        val password: String)
